7932f511b2d60a571a3ff16f7b3ad34a1506f87a
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-02-27 11:05:02 -0800

    Implementing some tests for IRC message parsing

--- lib/cameo_ex/irc_server/irc_message.ex
+++ lib/cameo_ex/irc_server/irc_message.ex
@@ -18,45 +18,84 @@ defmodule CameoEx.IrcServer.IrcMessage do
     params: [binary] | []
   }
 
+  @doc """
+  Parses `message` into an IrcMessage struct.
+
+  ## Examples
+
+      iex> parse_message(":foo NICK bar")
+      %IrcMessage{prefix: "foo", command: "NICK", params: ["bar"]}
+      iex> parse_message("USER zelda * * :Princess Zelda")
+      %IrcMessage{prefix: nil, command: "USER", params: ["zelda", "*", "*",
+                                                         "Princess Zelda"]}
+      iex> parse_message("QUIT")
+      %IrcMessage{prefix: nil, command: "QUIT", params: []}
+  """
   @spec parse_message(binary()) :: __MODULE__.t
   def parse_message(<<":", _::binary()>> = msg) do
     [prefix, rem] = get_prefix(msg)
-    %__MODULE__{prefix: prefix} |> struct(Map.from_struct(parse_message(rem)))
+    %{parse_message(rem)| prefix: prefix}
   end
 
   def parse_message(msg) do
     case msg |> String.split(" ", parts: 2) do
       [cmd| []] -> %__MODULE__{command: cmd, params: []}
-      [cmd, rem] -> %__MODULE__{command: cmd} |> struct(parse_params(rem))
+      [cmd, rem] -> %__MODULE__{command: cmd, params: parse_params(rem)}
     end
   end
 
-  @spec parse_params(binary()) :: %{params: [binary()]}
+  @doc """
+  Parse params from params part of IRC message `msg`.
+
+      iex> parse_params("one")
+      ["one"]
+      iex> parse_params("one two")
+      ["one", "two"]
+      iex> parse_params("one :two three")
+      ["one", "two three"]
+  """
+  @spec parse_params(binary()) :: [binary()]
   def parse_params(msg) do
     if String.contains?(msg, " :") do
       [middle, trailing] = String.split(msg, " :", parts: 2)
       mid_params = String.split(middle)
-      params = [mid_params, trailing] |> List.flatten
-      %{params: params}
+      [mid_params, trailing] |> List.flatten
     else
-      params = String.split(msg)
-      %{params: params}
+      String.split(msg)
     end
   end
 
   @doc """
   Get prefix from IRC message with prefix
+
+      iex> get_prefix(":foo BAR")
+      ["foo", "BAR"]
   """
   @spec get_prefix(binary()) :: list
-  def get_prefix(<<":", _::binary()>> = msg) do
+  def get_prefix(<<":", msg::binary()>> = _) do
     msg |> String.split(" ", parts: 2)
   end
 
+  @doc """
+  Build a message as though from a client.
+
+      iex> client = %CameoEx.IrcServer.ClientConnection{registered: true,
+      ...> nick: "nick", user: "user", host: "hostname"}
+      iex> build_client_msg(client, "TEST", ["one", "two"])
+      %IrcMessage{prefix: "nick!user@hostname", command: "TEST", params: ["one", "two"]}
+      iex> badclient = %{client| registered: false}
+      iex> build_client_msg(badclient, "TEST", ["one", "two"])
+      ** (ArgumentError) client must be registered
+  """
   @spec build_client_msg(ClientConnection.t,
                          binary(), [binary()]) :: __MODULE__.t
   def build_client_msg(%{registered: true} = client, command, params) do
     %__MODULE__{prefix: client_prefix(client), command: command,
                       params: params}
+  end
+
+  def build_client_msg(%{registered: false} = _, _, _) do
+    raise ArgumentError, "client must be registered"
   end
 
   # Kind of a misnomer - actually formats nick!user@host user format
---
+++ test/irc_message_test.exs
@@ -0,0 +1,5 @@
+defmodule IrcMessageTest do
+  use ExUnit.Case
+  alias CameoEx.IrcServer.IrcMessage
+  doctest CameoEx.IrcServer.IrcMessage, import: true
+end
